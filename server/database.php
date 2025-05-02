329f151992cdaa49fad7fb3d6ff5e9ab04972c86
candemarsan <candemarsan@gmail.com>
2025-05-02 18:23:35 +0200

    conexion base de datos y tablas

---
+++ server/database.php
@@ -0,0 +1,29 @@
+<?php
+
+/*---------------------------------------------------------------
+Variables de conexión
+---------------------------------------------------------------*/
+$server = "localhost";
+$user = "root";
+$password = "";
+
+// Petición de para la conexión
+$connection = mysqli_connect($server, $user, $password);
+
+/*---------------------------------------------------------------
+BASE DE DATOS
+---------------------------------------------------------------*/
+$sql = "SHOW DATABASES LIKE 'gastrolink'";
+$query = mysqli_query($connection, $sql) or die("ERROR: no se crea la Base de Datos");
+if (mysqli_fetch_array($query) <= 0) {
+    $sql = "CREATE DATABASE gastrolink";
+    if (mysqli_query($connection, $sql)) {
+        echo "";
+        mysqli_select_db($connection, 'gastrolink');
+        require_once "tablas.php";
+    } else {
+        echo "ERROR: no se crea la Base de Datos: " . mysqli_error($connection) . "<br>";
+    }
+} else {
+    mysqli_select_db($connection, 'gastrolink');
+}
